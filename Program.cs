7b3598cbad5336cbc34c71c3ecec35a6edfefee2
arielcollatti <colatti2017@gmail.com>
2023-05-05 22:21:42 -0300

    Exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.ForegroundColor =  ConsoleColor.DarkGreen;
+Console.WriteLine("--- Rolagem de Dado ---\n");
+Console.ResetColor();
+Console.Write("Quantas faces? ");
+int faces = int.Parse(Console.ReadLine());
+
+Random random = new Random();
+int resultado = random.Next(1, faces+1);
+Console.WriteLine("\n--- Rolagem de Dado ---\n");
+Console.WriteLine("Rolagem: " + resultado);
